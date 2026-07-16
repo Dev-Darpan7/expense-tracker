cb7b859881d27a72e96cb1017b275473c9b1a8e1
Darpan <imposterrr77@gmail.com>
2026-07-16 18:55:17 +0530

    Add expense rendering and total calculation

--- script.js
+++ script.js
@@ -16,24 +16,36 @@ let expenses = [
   },
 ];
 
-function addExpense(event) {
+let total = 0;
+const updateTotal = () => {
+  total = 0;
+  expenses.forEach((expense) => {
+    total = total + expense.amount;
+  });
+  totalAmount.value = total;// will update the total amount in the input field
+};
+
+// Function to add a new expense
+const addExpense = (event) => {
   event.preventDefault(); // Prevent the default form submission behavior
   const description = descriptionInput.value;
   const amount = parseFloat(amountInput.value);
   expenses.push({ name: description, amount: amount });
-    displayExpenses();
+  updateTotal();
+  displayExpenses();
   descriptionInput.value = "";
   amountInput.value = "";
-
 }
+
+
+// add event listener to the add expense button
 addExpenseBtn.addEventListener("click", addExpense);
 const displayExpenses = () => {
   expenseList.innerHTML = ""; // Clear the list before displaying expenses
   expenses.forEach((expense) => {
-     expenseList.innerHTML =
-    expenseList.innerHTML + `<li><span>${expense.name}</span><span>₹${expense.amount}</span></li>`;
-
+    expenseList.innerHTML =
+      expenseList.innerHTML +
+      `<li><span>${expense.name}</span><span>₹${expense.amount}</span></li>`;
   });
-}
+};
 displayExpenses();
-
