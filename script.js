c70efbc8c3609ce8f8bab730c743377d599a034e
Darpan <imposterrr77@gmail.com>
2026-07-19 21:29:28 +0530

    Add expense delete functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const totalAmount = document.getElementById("totalAmount");
 const amountInput = document.getElementById("amount");
 const descriptionInput = document.getElementById("expense-description");
 const expenseList = document.getElementById("expenses");
-const clearExpenseBtn= document.getElementById("clearExpense")
+const clearExpenseBtn = document.getElementById("clearExpense");
 
 let expenses = [
   { name: "Groceries", amount: 500 },
@@ -23,7 +23,7 @@ const updateTotal = () => {
   expenses.forEach((expense) => {
     total = total + expense.amount;
   });
-  totalAmount.value = total;// will update the total amount in the input field
+  totalAmount.value = total; // will update the total amount in the input field
 };
 
 // Function to add a new expense
@@ -36,19 +36,29 @@ const addExpense = (event) => {
   displayExpenses();
   descriptionInput.value = "";
   amountInput.value = "";
-}
-
+};
 
 // add event listener to the add expense button
 addExpenseBtn.addEventListener("click", addExpense);
+
 const displayExpenses = () => {
   expenseList.innerHTML = ""; // Clear the list before displaying expenses
-  expenses.forEach((expense) => {
+  expenses.forEach((expense, index) => {
     expenseList.innerHTML =
       expenseList.innerHTML +
-      `<li><span>${expense.name}</span><span>₹${expense.amount}</span></li>`;
+      `<li><span>${expense.name}</span><span>₹${expense.amount}</span><span><button  data-index="${index}">Delete</button></span></li>`;
+  });
+  const deleteButtons = document.querySelectorAll("button[data-index]");
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      expenses.splice(Number(button.dataset.index), 1);
+      updateTotal();
+      displayExpenses();
+    });
   });
 };
+
 clearExpenseBtn.addEventListener("click", () => {
   expenses = [];
   updateTotal();
